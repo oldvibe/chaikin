f654c44f211beec784de5f67cc9d059b1b120ac9
ASTA22 <135625795+oldvibe@users.noreply.github.com>
2025-08-05 18:12:01 +0100

    Update lib.rs
    push first and last element to tmp

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub fn chaikin_algo(in_points: &[(f32, f32)]) -> Vec<(f32, f32)> {
     let mut tmp:Vec<(f32, f32)> = Vec::new();
 
+    tmp.push(in_points[0]);
     for i in 0..in_points.len().saturating_sub(1) {
         let p0: (f32, f32) = in_points[i];
         let p1:(f32, f32) = in_points[i + 1];
@@ -17,6 +18,7 @@ pub fn chaikin_algo(in_points: &[(f32, f32)]) -> Vec<(f32, f32)> {
         tmp.push(q);
         tmp.push(r);
     }
+    tmp.push(*in_points.last().unwrap());
 
     tmp
 }
