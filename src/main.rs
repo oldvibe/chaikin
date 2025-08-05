6fc3c145a14fc74aeb7507809fa43dc16f38938c
ASTA22 <135625795+oldvibe@users.noreply.github.com>
2025-08-05 19:07:27 +0100

    Update main.rs
    fix first and last poonts update

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,12 @@ async fn main() {
     let mut current_step = 0;
     let mut timer = 0.0;
     let step_duration = 0.5;
-    // let smooth_points = chaikin_algo(&points);
-    // println!("{:?}", smooth_points);
     loop {
         clear_background(BLACK);
         if is_mouse_button_pressed(MouseButton::Left) {
             let (mx, my) = mouse_position();
             control_points.push((mx, my));
         }
-        // if is_mouse_button_pressed(MouseButton::Left)  {
-        //     println!("i'm in");
-        //     let (x, y) = mouse_position();
-        //     let first_point ==
-        //     points.push((x, y));
-        // }
 
         if is_key_pressed(KeyCode::Enter) && control_points.len() >= 1 {
             steps.clear();
@@ -58,20 +50,20 @@ async fn main() {
             &control_points
         };
 
-        // for &(x, y) in &points {
-        //     draw_circle(x, y, 5.0, YELLOW);
-        // }
+        for &(x, y) in &control_points {
+            draw_circle(x, y, 2.0, GRAY);
+        }
 
-        if control_points.len() >= 2 {
+        if !animating && control_points.len() >= 2 {
             for i in 0..control_points.len() - 1 {
                 let (x1, y1) = control_points[i];
                 let (x2, y2) = control_points[i + 1];
-                draw_line(x1, y1, x2, y2, 2.0, RED);
+                draw_line(x1, y1, x2, y2, 2.0, WHITE);
             }
         }
 
         for &(x, y) in points_to_draw {
-            draw_circle(x, y, 4.0, GREEN);
+            draw_circle(x, y, 4.0, GRAY);
         }
 
         if animating {
@@ -80,23 +72,12 @@ async fn main() {
                 for i in 0..points_to_draw.len() - 1 {
                     let (x1, y1) = points_to_draw[i];
                     let (x2, y2) = points_to_draw[i + 1];
-                    draw_line(x1, y1, x2, y2, 2.0, WHITE);
+                    draw_line(x1, y1, x2, y2, 2.0, DARKBLUE);
                 }
             }
         }
       
 
-        // ui.label(None, "Left");
-        // if is_mouse_button_down(MouseButton::Right) {
-        //     ui.label(None, "Right");
-        // }
-        // if is_mouse_button_down(MouseButton::Middle) {
-        //     ui.label(None, "Middle");
-        // }
-        // draw_line(200.0, 500.0, 500.0, 200.0, 1.0, BLUE);
-        // draw_line(500.0, 200.0, 40.0, 40.0, 1.0, BLUE);
-
-        // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
 
         next_frame().await;
     }
